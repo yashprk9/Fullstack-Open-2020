4a94d4efba83ce35dbae56157bb4790e253c4044
yashprk9 <yashpareek9399@gmail.com>
2020-11-27 23:19:44 +0530

    Updated

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,17 +1,10 @@
 import React from 'react'
 
-  const Header = ({ course }) => {
+const Header = ({ course }) => {
     return (
-      <h2>{course.name}</h2>
+        <h1>{course.name}</h1>
     )
-  }
-  
-  const Total = ({ course }) => {
-    const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-    return(
-      <p><b>total of {sum} exercises</b></p>
-    ) 
-  }
+}
   
   const Part = ({ part }) => {
     return (

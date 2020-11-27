274fa11297be3e6aac06b5a652267c7bd2e4a6a1
yashprk9 <yashpareek9399@gmail.com>
2020-11-28 00:46:46 +0530

    Updated

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -16,22 +16,18 @@ const Header = ({ course }) => {
   
   const Content = ({ course }) => {
     return (
-      <div>
-        {course.parts.map(part =>
-          <Part key={part.id} part={part}/>
-        )}
-      </div>
+        <div>
+        {course.parts.map((part) => <Part part={part}/>)}
+        </div>
     )
-  }
-  
-  const Course = ({ course }) => {
-    return (
-      <div>
-        <Header course={course} />
-        <Content course={course} />
-        <Total course={course} />
-      </div>
-    )
-  }
+}
 
-  export default Course
+export default function Course({course}) {
+    return(
+        <div>
+        <Header course={course}/>
+        <Content course={course}/>
+        <Total course={course}/>
+        </div>
+    )
+}

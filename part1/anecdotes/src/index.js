b853e566db3c3838e99b6b8f0a644eee6076baf5
yashprk9 <yashpareek9399@gmail.com>
2020-11-27 20:44:09 +0530

    updates

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -18,18 +18,16 @@ const App = ({anecdotes}) => {
   
       const maxVoted = () => votes.indexOf(Math.max(...votes));
       
-  
-
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-];
-
-ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
-)
+      return (
+        <div>
+            <h1>Anecdote of the day</h1>
+            <p>{anecdotes[selected]}</p>
+            <p>has {votes[selected]} votes</p>
+            <button onClick={handleVote}>Vote</button>
+            <button onClick={handleNextAnecdote}>Next Anecdote</button>
+            <h1>Anecdote with most votes</h1>
+            <p>{anecdotes[maxVoted()]}</p>
+            <p>has {votes[maxVoted()]} votes</p>
+        </div>
+    );
+};
